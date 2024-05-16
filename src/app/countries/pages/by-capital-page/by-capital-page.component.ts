b163ae71c2af0ed4cb3f9a7b987700fc8c9d2ac5
jose angel <jose.albelt1817@gmail.com>
2024-05-16 09:08:49 -0500

    service country

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CountryService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -7,10 +9,17 @@ import { Component } from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
+  public countries:Country[] = [];
+
+  constructor( private countryService:CountryService  ){
+
+  }
+
 
  searchByCapital( term:string): void{
-   console.log('Desde bycapitalPage');
-   console.log({term});
+   this.countryService.searchCapital(term).subscribe( contris => {
+      this.countries = contris;
+   })
  }
 
 
---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+
+@Injectable({providedIn: 'root'})
+
+
+export class CountryService {
+    
+    private  apiUrl: string = 'https://restcountries.com/v3.1';
+    
+    constructor( private _http: HttpClient  ) { }
+
+    
+    searchCapital( term: string ): Observable<Country[]>{
+       return this._http.get<Country[]>(`${this.apiUrl}/capital/${term}`);
+    }
+
+}
+
