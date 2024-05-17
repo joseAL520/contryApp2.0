2761170240c986706f0e55b111922c231d8a1c4b
jose angel <jose.albelt1817@gmail.com>
2024-05-17 09:38:43 -0500

    debouncer en by-capital sin usar enter

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,11 +1,14 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-components-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent  implements OnInit{
+  
+  private debouncer: Subject<string> = new Subject<string>();
 
   @Input()
   placeHolder:string = '';
@@ -13,9 +16,24 @@ export class SearchBoxComponent {
   @Output()
   onValue = new EventEmitter<string>();
 
+  @Output()
+  onDebounce = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncer
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe(value => {
+      this.onDebounce.emit(value);
+    })
+  }
 
   emitValue( value:string):void{
     this.onValue.emit(value)
   }
 
+  onKeyPress(searchTerm:string){
+    this.debouncer.next(searchTerm);
+  }
 }
